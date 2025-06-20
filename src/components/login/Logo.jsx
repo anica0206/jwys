f3d349fca48b279688b73d82c1473ac016b334a2
unknown <jiwoo.kim@astems.co.kr>
2025-06-20 09:49:06 +0900

    로그인 v2.0

---
+++ src/components/login/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Memo from './Memo';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="memo-with-logo-container">
+      <img src="/images/linkus.png" alt="Linkus Logo" className="memo-logo" />
+      {/* <Memo /> */}
+    </div>
+  );
+};
+
+export default Logo;
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { login } from '../../api/authApi';
 import { useNavigate } from 'react-router-dom';
-import SignupModal from './Signup.jsx';  // 모달 컴포넌트 불러오기
+import SignupModal from './Signup.jsx';
+import LoginMemo from '../../components/login/LoginMemo'; // Memo 컴포넌트 추가
+import Logo from '../../components/login/Logo';
 
 const Login = () => {
   const [id, setId] = useState(localStorage.getItem('id') || '');
@@ -30,13 +32,9 @@ const Login = () => {
     <section className="min-h-screen flex items-center">
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 w-full">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-5 md:items-center md:gap-8">
-
-          <div className="md:col-span-3">
-            <img
-              src="https://images.unsplash.com/photo-1731690415686-e68f78e2b5bd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="rounded w-full h-auto"
-              alt=""
-            />
+          <div className="md:col-span-3 flex flex-col items-center justify-center gap-6">
+            <Logo />
+            <LoginMemo />
           </div>
 
           <div className="md:col-span-2 flex flex-col items-center justify-center px-6 py-8">
@@ -47,24 +45,18 @@ const Login = () => {
                 </h1>
                 <form className="space-y-4 md:space-y-6">
                   <div>
-                    {/* <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                      아이디
-                    </label> */}
                     <input
                       type="email"
                       name="email"
                       id="email"
                       value={id}
                       onChange={(e) => setId(e.target.value)}
-                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white font-gamja"
                       placeholder="아이디"
                       required
                     />
                   </div>
                   <div>
-                    {/* <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                      비밀번호
-                    </label> */}
                     <input
                       type="password"
                       name="password"
@@ -72,7 +64,7 @@ const Login = () => {
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                       placeholder="비밀번호"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white font-gamja"
                       required
                     />
                   </div>
